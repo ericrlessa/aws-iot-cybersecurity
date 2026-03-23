069b22882ead8c36364bdb66c29f90489ae16a46
Eric Rosetti Lessa <ericrlessa@gmail.com>
2026-03-23 13:48:26 -0400

    improved mosquitto class

--- mosquitto.py
+++ mosquitto.py
@@ -1,9 +1,10 @@
 import paho.mqtt.client as mqtt  # For EC2 Mosquitto 
 import logging
+import os
 
-EC2_HOST = "<IP>"
-EC2_PORT = 1883 
-EC2_TOPIC = "temp/data"
+MOSQUITTO_HOST = os.environ.get('MOSQUITTO_HOST', 'localhost')
+MOSQUITTO_PORT = 1883 
+MOSQUITTO_TOPIC = "temp/data"
 
 class EC2MQTTClient:
     def __init__(self):
@@ -12,13 +13,15 @@ class EC2MQTTClient:
         self.connected = False
 
     def on_connect(self, client, userdata, flags, rc):
+        logging.info("connected to mosquitto broker!")
         self.connected = True
 
     def connect(self):
-        self.client.connect(EC2_HOST, EC2_PORT)
+        logging.info("Starting connection to mosquitto broker...")
+        self.client.connect(MOSQUITTO_HOST, MOSQUITTO_PORT)
         self.client.loop_start()
 
     def publish(self, payload):
         if not self.connected:
             raise Exception("Not connected")
-        self.client.publish(EC2_TOPIC, payload)
+        self.client.publish(MOSQUITTO_TOPIC, payload)
